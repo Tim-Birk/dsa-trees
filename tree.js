6050ca372c484dea85424297ef8ac9eb13061aec
Tim Birkmire <timbirkmire@yahoo.com>
2021-12-26 10:41:26 -0500

    finish tree.js and make sure tests are passing

--- tree.js
+++ tree.js
@@ -15,20 +15,65 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (!this.root) {
+      return 0;
+    }
+    let treeStack = [this];
+    let sum = 0;
+
+    while (treeStack.length) {
+      let current = treeStack.pop();
+      current = current.root ? current.root : current;
+      sum += current.val;
+
+      for (let child of current.children) treeStack.push(child);
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if (!this.root) {
+      return 0;
+    }
 
+    let treeStack = [this];
+    let count = 0;
+
+    while (treeStack.length) {
+      let current = treeStack.pop();
+      current = current.root ? current.root : current;
+      if (current.val % 2 === 0) {
+        count++;
+      }
+
+      for (let child of current.children) treeStack.push(child);
+    }
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (!this.root) {
+      return 0;
+    }
 
+    let treeStack = [this];
+    let count = 0;
+
+    while (treeStack.length) {
+      let current = treeStack.pop();
+      current = current.root ? current.root : current;
+      if (current.val > lowerBound) {
+        count++;
+      }
+
+      for (let child of current.children) treeStack.push(child);
+    }
+    return count;
   }
 }
 
